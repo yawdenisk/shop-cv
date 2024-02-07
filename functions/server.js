4bd0b4388cfd97c92f2c790a2d593e55d5162e6b
Denys Ivashchenko <yawdenisk@gmail.com>
2024-02-07 11:41:26 +0100

    test3

--- functions/server.js
+++ functions/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const cors = require('cors'); // Добавим модуль для обработки CORS
+const cors = require('cors');
 const nodemailer = require('nodemailer');
-const serverless = require('serverless-http'); 
+const serverless = require('serverless-http');
+
 const app = express();
 
-// Добавим использование CORS
 app.use(cors());
 app.use(express.json());
 
@@ -16,34 +16,35 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-app.post('/send-email', async (req, res) => { // Обработчик POST-запроса для отправки письма
-  const { orders, count, userName, userSurName, userMail, userCity, userPost, userPhone } = req.body;
-
-  if (!orders) {
-    return res.status(400).send('Bad Request: Orders data is missing');
-  }
-
-  const emailBody = orders.map(order => `${order.title} - ${order.quantity} шт.`).join('\n') + '\n' + 'Загальна ціна :' + count.toFixed(2) + '\n' +
-    'Ім\'я:  ' + userName + '\n' +
-    'Прізвище:  ' + userSurName + '\n' +
-    'Телефон:  ' + userPhone + '\n' +
-    'Пошта:  ' + userMail + '\n' +
-    'Поштовий індекс:  ' + userPost + '\n' +
-    'Місто:  ' + userCity + '\n';
-
-  const mailOptions = {
-    from: 'shop.dla.tebe@gmail.com',
-    to: 'shop.dla.tebe@gmail.com',
-    subject: 'Нове замовлення',
-    text: emailBody,
-  };
-
+app.post('/send-email', async (req, res) => {
   try {
-    await transporter.sendMail(mailOptions); // Отправляем письмо
-    res.status(200).send('Email sent successfully'); // Ответ в случае успешной отправки
+    const { orders, count, userName, userSurName, userMail, userCity, userPost, userPhone } = req.body;
+
+    if (!orders) {
+      return res.status(400).json({ error: 'Bad Request: Orders data is missing' });
+    }
+
+    const emailBody = orders.map(order => `${order.title} - ${order.quantity} шт.`).join('\n') + '\n' + 
+                      `Загальна ціна: ${count.toFixed(2)}\n` + 
+                      `Ім'я: ${userName}\n` +
+                      `Прізвище: ${userSurName}\n` +
+                      `Телефон: ${userPhone}\n` +
+                      `Пошта: ${userMail}\n` +
+                      `Поштовий індекс: ${userPost}\n` +
+                      `Місто: ${userCity}\n`;
+
+    const mailOptions = {
+      from: 'shop.dla.tebe@gmail.com',
+      to: 'shop.dla.tebe@gmail.com',
+      subject: 'Нове замовлення',
+      text: emailBody,
+    };
+
+    await transporter.sendMail(mailOptions);
+    res.status(200).send('Email sent successfully');
   } catch (error) {
     console.error(error);
-    res.status(500).send('Error sending email'); // Ответ в случае ошибки
+    res.status(500).send('Error sending email');
   }
 });
 
