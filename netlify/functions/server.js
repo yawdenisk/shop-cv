f82956879869880e18a9251b6300fd58bb65a67f
Denys Ivashchenko <yawdenisk@gmail.com>
2024-02-10 16:28:01 +0100

    test

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -53,4 +53,8 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something went wrong!');
 });
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 module.exports.handler = serverless(app);
